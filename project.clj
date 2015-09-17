ba19474da73461905241e8049b7226088bc575be
Vuk Mirovic <wooque@gmail.com>
2015-09-17 23:57:38 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject github-trending "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [enlive "1.1.5"]
-                 [com.draines/postal "1.11.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [enlive "1.1.6"]]
   :main ^:skip-aot github-trending.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
